34b750efac8d49bac556297c5dddb7bff60f6af4
Kishan-panday <142047807+Kishan-panday@users.noreply.github.com>
2023-09-01 19:02:04 +0000

    .

---
+++ java script/sec.js
@@ -0,0 +1,9 @@
+let kishan=18
+console.log(kishan)
+let age =promt("what is your age?")
+if (age>10 && age<20){
+    console.log("your age lies between 10 and 20")
+}
+else{
+    console.log("your age does not lies between 10 and 20")
+}
