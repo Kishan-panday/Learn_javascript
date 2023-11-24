3cc993877df672824c6e455b740c679912ee1652
Kishan-Panday <kishanpandey4463@gmail.com>
2023-11-24 23:32:24 +0530

    dictionary

---
+++ dict.js
@@ -0,0 +1,7 @@
+const dict={
+    stipend:"an amount of money that is paid regularly to somebody as wages or an allowance usually for some specific purpose\n",
+
+    disparage:"to talk about somebody/something in a critical way; to say that somebody/something is of little value or importance",
+   
+ }
+  console.log(dict.stipend, dict.disparage)
