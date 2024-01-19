8ff6b328c2d8d759e9d783127e6abc433a32797a
Kishan-Panday <kishanpandey4463@gmail.com>
2024-01-19 23:58:00 +0530

    name

--- java script/fun.js
+++ java script/fun.js
@@ -1,7 +1,8 @@
 function oneplusavg (x,y){
     return 1 + (x+y)/2
 }
-let a=1,b=2,c=3;
+let a=1;b=2;c=3;
 console.log(oneplusavg(a,b))
 console.log(oneplusavg(b,c))
-console.log(oneplusavg(c,a))
+let  friend = "kish\an";
+console.log(friend)
---
+++ java script/name.js
@@ -0,0 +1,6 @@
+let name ="kishan"
+console.log(name.length)
+console.log(name.toUpperCase())
+console.log(name.toLowerCase())
+console.log(name.slice(2,5))
+console.log(name.slice(2))
