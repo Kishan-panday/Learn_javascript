ab0e2424069f18f3332311ae194977df9f34f08c
Kishan-Panday <kishanpandey4463@gmail.com>
2024-01-28 16:56:50 +0530

    array

---
+++ java script/array.js
@@ -0,0 +1,13 @@
+let num=[3,4,45,78,23,3435,5457,4457,09,987]
+let compare=(a,b)=>{
+    return a-b
+}
+num.sort()
+console.log(num)
+num.sort(compare)
+
+console.log(num)
+
+
+num.splice(3,1,10,56,224)
+console.log(num)
