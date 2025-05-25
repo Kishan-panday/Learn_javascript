4478a5372aa58bdb59bc9edd45b66a888dba8c80
Kishan-Panday <kishanpandey4463@gmail.com>
2025-05-25 14:36:16 +0530

    guess the number

---
+++ ps/ps5.js
@@ -0,0 +1,8 @@
+for(let i = 0;i<101;i++){
+    if(i%2===0){
+        console.log(i,"even number"); 
+    }else{
+        console.log(i,"Odd number");
+    }
+   
+}
---
+++ ps/ps6.js
@@ -0,0 +1,10 @@
+let num = prompt("Enter the number");
+let i = 8;
+console.log(num);
+while (num!=i) {
+    num = prompt("Try again.Enter the number");
+    console.log(num);
+}
+
+    console.log(num,"you have enter the right value");
+
